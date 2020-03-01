68ef6c297eec681b23fc15e922147ffe56c01115
ktw1016 <ktw_1016@hotmail.com>
2020-03-01 05:54:54 -0500

    minor code clean up

--- src/components/matrix.js
+++ src/components/matrix.js
@@ -33,37 +33,39 @@ export default class Matrix extends React.Component {
 
     return <div>
       <label> Search APT by: </label>
-      <select onChange={(evt) => {
-        this.setState({ query_criteria: evt.target.value })
-      }}>
-        <option value="id" selected> ID </option>
+      <select onChange={(evt) =>  this.setState({ query_criteria: evt.target.value }) }>
+        <option value="id" defaultValue> ID </option>
         <option value="name"> Name </option>
         <option value="created"> Created </option>
-        <option value="x_mitre_version"> X Mitre Version </option>
+        <option value="x_mitre_version"> Version </option>
         <option value="type"> Type </option>
         <option value="description"> Description </option>
       </select>
       <input
         type="text"
         placeholder="Search APT"
-        onChange={(evt) => {
-          this.setState({ query: evt.target.value });
-        }}
+        onChange={ (evt) => this.setState({ query: evt.target.value }) }
       />
+      <br></br>
+      <span style={{ fontWeight: 100, fontSize: 12 }}>
+        Showing {zipped_data.length} rows of data
+      </span>
       <table>
-        <tr key={"atp_type_header"}>
-          {_.map(processed_data, (atp_data, atp_type) => 
-            <td style={{fontWeight: "bold"}}> {atp_type} </td>
-          )}
-        </tr>
-        {_.map(zipped_data, (row) => 
-          <tr>
-            { _.map(row, (data) =>
-              data &&
-              <td key={data.id}> { data.name ? data.name : data.id } </td>
+        <tbody>
+          <tr key={"atp_type_header"}>
+            {_.map(processed_data, (atp_data, atp_type) => 
+              <td key={atp_type} style={{fontWeight: "bold"}}> {atp_type} </td>
             )}
           </tr>
-          )}
+          {_.map(zipped_data, (row) => 
+            <tr key={_.uniqueId()}>
+              { _.map(row, (data) =>
+                data &&
+                <td key={data.id}> { data.name ? data.name : data.id } </td>
+              )}
+            </tr>
+            )}
+        </tbody>
       </table>
     </div>;
   }
